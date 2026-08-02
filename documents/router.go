f67ce9fdac61b75477276d3b79634123ad4e857a
Afa <afa.alone@gmail.com>
2026-08-03 00:14:38 +0300

    gorm prepared

--- documents/router.go
+++ documents/router.go
@@ -1,8 +1,31 @@
 package documents
 import (
+	"net/http"
 	"github.com/labstack/echo/v5"
 )
 
 func Routes(g *echo.Group) {
+	g.GET("/", GetDocuments)
+	g.GET("/:id", GetDocument)
+	g.POST("/", CreateDocument)
+	g.PUT("/:id", UpdateDocument)
+	g.DELETE("/:id", DeleteDocument)
 	return
+}
+
+func GetDocuments(c *echo.Context) error {
+	return c.JSON(http.StatusOK, []string{"document1", "document2"})
+}
+
+func GetDocument(c *echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"document": "document1"})
+}
+func CreateDocument(c *echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"message": "document created"})
+}
+func UpdateDocument(c *echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"message": "document updated"})
+}
+func DeleteDocument(c *echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"message": "document deleted"})
 }
--- main.go
+++ main.go
@@ -3,12 +3,32 @@ package main
 import (
 	// "fmt"
 	"app/documents"
+	"net/http"
+
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
-	"net/http"
+
+	// orm
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Document struct {
+	gorm.Model
+	Text string
+}
+
 func main() {
+	setup_db()
+	e := setup_routes()
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
+
+func setup_routes() *echo.Echo {
 	e := echo.New()
 
 	e.Use(middleware.RequestLogger())
@@ -16,12 +36,20 @@ func main() {
 
 	docs := e.Group("documents")
 	documents.Routes(docs)
-	
-	e.GET("/", rootHandler)
 
-	if err := e.Start(":1323"); err != nil {
-		e.Logger.Error("failed to start server", "error", err)
+	e.GET("/", rootHandler)
+	return e
+}
+
+func setup_db() *gorm.DB {
+	db, err := gorm.Open(postgres.Open("host=localhost dbname=fw_dev sslmode=disable"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
 	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Document{})
+	return db
 }
 
 func rootHandler(c *echo.Context) error {
